ec8a1e3cf5d2856678ac708cd64a39e89fac94aa
Ian Campbell <racecar333@gmail.com>
2016-04-30 15:29:17 -0400

    Section 4, Lecture 25:  Creating a unit test for password-protected resource - fails on 401 Unauthorized, credentials must be supplied every time a method is called

--- src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
+++ src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
@@ -7,6 +7,14 @@ import javax.ws.rs.core.MediaType;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import com.google.common.base.Optional;
+import com.udemy.dropbookmarks.core.User;
+
+import io.dropwizard.auth.AuthFactory;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.testing.junit.ResourceTestRule;
 
 /**
@@ -14,9 +22,21 @@ import io.dropwizard.testing.junit.ResourceTestRule;
  * @author icampbell2
  */
 public final class HelloResourceTest {
+	
+	/**
+	 * 
+	 */
+	private static final Authenticator<BasicCredentials, User> AUTHENTICATOR = new Authenticator<BasicCredentials, User>() {
+
+		@Override
+		public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+			return Optional.of(new User());
+		}		
+	};
 
 	@ClassRule
 	public static final ResourceTestRule RULE = ResourceTestRule.builder()
+			.addProvider(AuthFactory.binder(new BasicAuthFactory<>(AUTHENTICATOR, "realm", User.class)))
 			.addResource(new HelloResource())
 			.build();
 	
