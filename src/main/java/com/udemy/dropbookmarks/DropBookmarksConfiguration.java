2a77d684056337e3a778ee89a352b0a08fd3d4d2
Ian Campbell <racecar333@gmail.com>
2016-05-01 13:59:56 -0400

    Section 5, Lecture 34:  Configuring Database Connection

--- src/main/java/com/udemy/dropbookmarks/DropBookmarksConfiguration.java
+++ src/main/java/com/udemy/dropbookmarks/DropBookmarksConfiguration.java
@@ -1,18 +1,30 @@
 package com.udemy.dropbookmarks;
 
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
 
 public class DropBookmarksConfiguration extends Configuration {
-	
+
 	/**
 	 * 
 	 */
 	@NotEmpty
 	private String password;
-	
+
+	/**
+	 * 
+	 */
+	@NotNull
+	@Valid
+	private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
 	/**
 	 * 
 	 * @return
@@ -20,5 +32,14 @@ public class DropBookmarksConfiguration extends Configuration {
 	@JsonProperty
 	public String getPassword() {
 		return password;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@JsonProperty("database")
+	public DataSourceFactory getDataSourceFactory() {
+		return dataSourceFactory;
 	}
 }
