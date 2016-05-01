ddffd9f1b2e0c444137ad723027d3dd67c6b4d6a
Ian Campbell <racecar333@gmail.com>
2016-05-01 02:44:25 -0400

    Section 5, Lecture 32:  Integration Testing continued

--- src/test/java/com/udemy/dropbookmarks/auth/AuthIntegrationTest.java
+++ src/test/java/com/udemy/dropbookmarks/auth/AuthIntegrationTest.java
@@ -2,10 +2,14 @@ package com.udemy.dropbookmarks.auth;
 
 import static org.junit.Assert.*;
 
+import java.util.ResourceBundle;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -13,6 +17,7 @@ import org.junit.Test;
 
 import com.udemy.dropbookmarks.DropBookmarksApplication;
 import com.udemy.dropbookmarks.DropBookmarksConfiguration;
+import com.udemy.dropbookmarks.resources.Hello;
 
 import io.dropwizard.testing.junit.DropwizardAppRule;
 
@@ -37,12 +42,23 @@ public final class AuthIntegrationTest {
 	/**
 	 * 
 	 */
-	private static final String TARGET = "http://localhost:8080";
+	private static final HttpAuthenticationFeature FEATURE
+			= HttpAuthenticationFeature.basic("username", "p@ssw0rd");
 	
 	/**
 	 * 
 	 */
-	private static final String PATH = "/hello/secured";
+	private static final String TARGET = "http://localhost:8080/";
+	
+	/**
+	 * 
+	 */
+	private static final Hello helloSecuredWorld = Hello.HELLO_SECURED_WORLD;
+	
+	/**
+	 * 
+	 */
+	private static final String PATH = helloSecuredWorld.getPath();
 	
 	/**
 	 * 
@@ -69,7 +85,7 @@ public final class AuthIntegrationTest {
 	 * 
 	 */
 	@Test
-	public void testPath() {
+	public void testSecuredPath() {
 		Response response = client.target(TARGET)
 				.path(PATH)
 				.request()
@@ -78,5 +94,20 @@ public final class AuthIntegrationTest {
 		assertEquals(Response.Status
 				.UNAUTHORIZED.getStatusCode(),
 				response.getStatus());
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testSecuredGreeting() {
+		client.register(FEATURE);
+		
+		String result = client.target(TARGET)
+				.path(PATH)
+				.request(MediaType.TEXT_PLAIN)
+				.get(String.class);
+		
+		assertEquals(result, helloSecuredWorld.getGreeting());
 	}
 }
