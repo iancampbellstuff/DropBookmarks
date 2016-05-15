176fc7435af9247116118498853a14d78276598d
Ian Campbell <racecar333@gmail.com>
2016-05-15 12:18:37 -0400

    Section 7, Lecture 50:  Adding Hibernate support to the Application class

--- src/main/java/com/udemy/dropbookmarks/DropBookmarksApplication.java
+++ src/main/java/com/udemy/dropbookmarks/DropBookmarksApplication.java
@@ -2,6 +2,7 @@ package com.udemy.dropbookmarks;
 
 import com.udemy.dropbookmarks.auth.HelloAuthenticator;
 import com.udemy.dropbookmarks.core.User;
+import com.udemy.dropbookmarks.db.UserDAO;
 import com.udemy.dropbookmarks.resources.HelloResource;
 
 import io.dropwizard.Application;
@@ -9,12 +10,24 @@ import io.dropwizard.Configuration;
 import io.dropwizard.auth.AuthFactory;
 import io.dropwizard.auth.basic.BasicAuthFactory;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class DropBookmarksApplication extends Application<DropBookmarksConfiguration> {
 
+	/**
+	 * 
+	 */
+	private final HibernateBundle<DropBookmarksConfiguration> hibernateBundle
+			= new HibernateBundle<DropBookmarksConfiguration>(User.class) {
+				@Override
+				public DataSourceFactory getDataSourceFactory(DropBookmarksConfiguration configuration) {
+					return configuration.getDataSourceFactory();
+				}
+	};
+	
     public static void main(final String[] args) throws Exception {
         new DropBookmarksApplication().run(args);
     }
@@ -35,6 +48,8 @@ public class DropBookmarksApplication extends Application<DropBookmarksConfigura
 				return configuration.getDataSourceFactory();
 			}
     	});
+    	
+    	bootstrap.addBundle(hibernateBundle);
     }
 	
     /**
@@ -42,6 +57,7 @@ public class DropBookmarksApplication extends Application<DropBookmarksConfigura
      */
 	@Override
 	public void run(final DropBookmarksConfiguration configuration, final Environment environment) {
+		final UserDAO userDAO = new UserDAO(hibernateBundle.getSessionFactory());
 		
 		// registering resources
 		environment.jersey().register(new HelloResource());
