dc72dc555e1246180b21e15a5583924445d7eef0
Ian Campbell <racecar333@gmail.com>
2016-06-05 10:46:34 -0400

    Section 8, Lecture 57: How Dropwizard deals with exceptions in resource methods

--- src/main/java/com/udemy/dropbookmarks/resources/BookmarksResource.java
+++ src/main/java/com/udemy/dropbookmarks/resources/BookmarksResource.java
@@ -3,11 +3,13 @@ package com.udemy.dropbookmarks.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.google.common.base.Optional;
 import com.udemy.dropbookmarks.core.Bookmark;
 import com.udemy.dropbookmarks.core.User;
 import com.udemy.dropbookmarks.db.BookmarkDAO;
@@ -30,5 +32,15 @@ public final class BookmarksResource {
 	@UnitOfWork
 	public List<Bookmark> getBookmarks(@Auth User user) {
 		return dao.findForUser(user.getId());
+	}
+	
+	private Optional<Bookmark> findIfAuthorized(long bookmarkId, long userId) {
+		Optional<Bookmark> result = dao.findById(bookmarkId);
+		
+		if (result.isPresent() && userId != result.get().getUser().getId()) {
+			throw new ForbiddenException("You are not authorized to see this resource.");
+		}
+		
+		return result;
 	}
 }
